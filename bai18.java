eb50e805ae497200e771540949a1be972366faae
pqhuy996 <phanquochuy99206@gmail.com>
2024-10-05 23:07:37 +0700

    baitapOOPchap3

---
+++ bai18.java
@@ -0,0 +1,5 @@
+public class bai18 {
+    public static void main(String[] args) {
+        System.out.println("Đề lỏ quá e không biết làm :))");
+    }
+}
---
+++ bai22.java
@@ -0,0 +1,8 @@
+public class bai22 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 0777; i++) {
+            // In ra số ở dạng hệ bát phân
+            System.out.printf("%03o\n", i);
+        }
+    }
+}
